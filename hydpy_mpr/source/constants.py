256f2553ee671d43516beccabc1707dffa5fcba5
Christoph Tyralla <c.tyralla@bjoernsen.de>
2024-10-07 21:24:09 +0200

    Add module `constants` to subpackage `source` to keep all (central) constants in one place.

---
+++ hydpy_mpr/source/constants.py
@@ -0,0 +1,11 @@
+"""This module contains all constants defined by HydPy-MPR."""
+
+# Table for mapping element IDs to element names required when working with raster
+# files:
+MAPPING_TABLE = "element_id2name"
+
+# Name of the mapping table's element ID column:
+ELEMENT_ID = "element_id"
+
+# Name of the mapping table's element name column:
+ELEMENT_NAME = "element_name"
