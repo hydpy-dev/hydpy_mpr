2e3f0aa9d220a40ecbd319a3a1c1748e74822fb0
Christoph Tyralla <c.tyralla@bjoernsen.de>
2024-10-28 14:01:43 +0100

    Support the POSIX path separator also when executing doctests on Windows.

--- hydpy_mpr/__init__.py
+++ hydpy_mpr/__init__.py
@@ -1,1 +1,22 @@
+import os
+
+
+from hydpy_mpr import testing
+
+
 __version__ = "0.0.dev0"
+
+
+if os.path.exists(os.path.join(testing.__path__[0], ".hydpy_mpr_doctest_hack")):
+    # Support the POSIX path separator also when executing doctests on Windows:
+
+    from doctest import OutputChecker
+
+    _original = OutputChecker._toAscii  # type: ignore[attr-defined]  # pylint: disable=protected-access
+
+    def _modified(self: OutputChecker, s: str) -> str:
+        s = _original(self, s)
+        return s.replace("\\", "/")
+
+    OutputChecker._toAscii = _modified  # type: ignore[attr-defined]  # pylint: disable=protected-access
+
