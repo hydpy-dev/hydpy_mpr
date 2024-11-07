2bb2d841098582525a57ff67ce25a3b39cf3acd4
Christoph Tyralla <c.tyralla@bjoernsen.de>
2024-11-07 17:15:05 +0100

    Let black also format the (Python) configuration files in the `data` subpackage.

--- hydpy_mpr/data/HydPy-H-Lahn/config/equations.py
+++ hydpy_mpr/data/HydPy-H-Lahn/config/equations.py
@@ -16,9 +16,7 @@ class FC(mpr.RasterEquation):
 
     def apply_coefficients(self) -> None:
         self.output[:] = (
-                self.coef_const.value
-                + self.coef_factor_sand.value * self.data_sand.values
-                + self.coef_factor_clay.value * self.data_clay.values
+            self.coef_const.value
+            + self.coef_factor_sand.value * self.data_sand.values
+            + self.coef_factor_clay.value * self.data_clay.values
         )
-
-
