1a01077a77df5f112edd381afcaed2ed06c9728e
tyralla <c.tyralla@bjoernsen.de>
2024-11-18 12:15:35 +0100

    Extract the general raster equation functionalities of class `RasterRegionaliser` into its new base class `RasterEquation`.

---
+++ hydpy_mpr/source/equations.py
@@ -0,0 +1,55 @@
+from __future__ import annotations
+import abc
+import dataclasses
+
+import numpy
+
+from hydpy_mpr.source import reading
+from hydpy_mpr.source.typing_ import *
+
+
+@dataclasses.dataclass
+class RasterEquation(abc.ABC):
+
+    dir_group: str
+    group: reading.RasterGroup = dataclasses.field(init=False)
+    mask: MatrixFloat = dataclasses.field(init=False)
+    output: MatrixFloat = dataclasses.field(init=False)
+
+    def activate(self, *, raster_groups: reading.RasterGroups) -> None:
+        group = raster_groups[self.dir_group]
+        self.group = group
+        self.mask = numpy.full(self.shape, True, dtype=bool)
+        for fieldname, filename in self.fieldname2filename.items():
+            rastername = f"data_{fieldname.removeprefix('file_')}"
+            raster = group.data_rasters[filename]
+            setattr(self, rastername, raster)
+            self.mask *= raster.mask
+        self.output = numpy.full(self.shape, numpy.nan)
+
+    @property
+    def shape(self) -> tuple[int, int]:
+        shape = self.group.shape
+        assert isinstance(shape, tuple)
+        assert len(shape) == 2
+        return shape
+
+    @property
+    def fieldname2filename(self) -> dict[str, str]:
+        return {
+            field.name: getattr(self, field.name)
+            for field in dataclasses.fields(self)
+            if (field.name).startswith("file_")
+        }
+
+    @property
+    def inputs(self) -> Mapping[str, reading.RasterFloat]:
+        return {
+            name: value
+            for field in dataclasses.fields(self)
+            if ((name := field.name) != "output")
+            and isinstance(value := getattr(self, name), reading.RasterFloat)
+        }
+
+    def apply_mask(self) -> None:
+        self.output[~self.mask] = numpy.nan
--- hydpy_mpr/source/regionalising.py
+++ hydpy_mpr/source/regionalising.py
@@ -4,7 +4,7 @@ import dataclasses
 
 import numpy
 
-from hydpy_mpr.source import reading
+from hydpy_mpr.source import equations
 from hydpy_mpr.source.typing_ import *
 
 
@@ -36,47 +36,7 @@ class Coefficient:
 
 
 @dataclasses.dataclass
-class RasterRegionaliser(abc.ABC):
-
-    dir_group: str
-    group: reading.RasterGroup = dataclasses.field(init=False)
-    mask: MatrixFloat = dataclasses.field(init=False)
-    output: MatrixFloat = dataclasses.field(init=False)
-
-    def activate(self, *, raster_groups: reading.RasterGroups) -> None:
-        group = raster_groups[self.dir_group]
-        self.group = group
-        self.mask = numpy.full(self.shape, True, dtype=bool)
-        for fieldname, filename in self.fieldname2filename.items():
-            rastername = f"data_{fieldname.removeprefix('file_')}"
-            raster = group.data_rasters[filename]
-            setattr(self, rastername, raster)
-            self.mask *= raster.mask
-        self.output = numpy.full(self.shape, numpy.nan)
-
-    @property
-    def shape(self) -> tuple[int, int]:
-        shape = self.group.shape
-        assert isinstance(shape, tuple)
-        assert len(shape) == 2
-        return shape
-
-    @property
-    def fieldname2filename(self) -> dict[str, str]:
-        return {
-            field.name: getattr(self, field.name)
-            for field in dataclasses.fields(self)
-            if (field.name).startswith("file_")
-        }
-
-    @property
-    def inputs(self) -> Mapping[str, reading.RasterFloat]:
-        return {
-            name: value
-            for field in dataclasses.fields(self)
-            if ((name := field.name) != "output")
-            and isinstance(value := getattr(self, name), reading.RasterFloat)
-        }
+class RasterRegionaliser(equations.RasterEquation, abc.ABC):
 
     @property
     def coefficients(self) -> tuple[Coefficient, ...]:
@@ -89,6 +49,3 @@ class RasterRegionaliser(abc.ABC):
     @abc.abstractmethod
     def apply_coefficients(self) -> None:
         pass
-
-    def apply_mask(self) -> None:
-        self.output[~self.mask] = numpy.nan
